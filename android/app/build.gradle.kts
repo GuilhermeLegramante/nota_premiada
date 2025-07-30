65ff1c49614a539385b3c10f4d71c16d4eba99a3
GuilhermeLegramante <guilhermelegramante@gmail.com>
2025-07-30 13:59:26 -0300

    1.1.0

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.hardsoft.notaPremiada"
         minSdk = 21
         targetSdk = 35
-        versionCode = 3
-        versionName = "1.0.2"
+        versionCode = 4
+        versionName = "1.1.0"
     }
 
     signingConfigs {
